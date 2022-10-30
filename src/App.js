54261f8fc1575b076f96f8ad07cbe51ed38678b1
Luis Alonso Alvarez Aguilar <l.alvarezaguilar448@gmail.com>
2022-10-30 10:21:48 -0500

    Auth

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Chat from './components/Chat';
 
-import { auth } from './firebase';
+import { auth } from "firebase/auth";
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 const style = {
